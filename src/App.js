ce28ab45d3cdf779f3926198df19a269e05540ba
nhatvy1 <86657972+nhatvy1@users.noreply.github.com>
2023-05-08 14:28:11 +0700

    header

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import logo from './logo.svg';
 import './App.scss';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-import MyComponent from './components/MyComponent';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header/Header';
 
 const App = () => {
 	return (
 		<div className="App">
-			Hello world
-			<MyComponent />
+			<Header />
 		</div>
 	)
 }
