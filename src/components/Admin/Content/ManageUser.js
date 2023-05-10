8acf369d0f0704970b604ebddac0baad57636b9e
nhatvy1 <86657972+nhatvy1@users.noreply.github.com>
2023-05-10 15:00:23 +0700

    design modal update user

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -4,9 +4,13 @@ import { FcPlus } from 'react-icons/fc';
 import { useState, useEffect } from "react";
 import TableUser from "./TableUser";
 import { getAllUsers } from '../../../services/apiService';
+import ModalUpdateUser from "./ModalUpdateUser";
 
 const ManageUser = (props)=> {
     const [showModalCreateUser, setShowModalCreateUser] = useState(false)
+    const [showModalUpdateUser, setShowModalUpdateUser] = useState(false)
+    const [dataUpdate, setDataUpdate] = useState({})
+
     const [listUser, setListUser] = useState([])
 
     useEffect(()=> {
@@ -21,6 +25,11 @@ const ManageUser = (props)=> {
         }
     }
 
+    const handleClickBtnUpdate = (user)=> {
+        setShowModalUpdateUser(true)
+        setDataUpdate(user)
+    }
+
     return(
         <div className="manage-user-container">
             <div className="title">
@@ -33,11 +42,17 @@ const ManageUser = (props)=> {
                 <div className="table-users-container">
                     <TableUser 
                         listUser={listUser}
+                        handleClickBtnUpdate={handleClickBtnUpdate}
                     />
                     <ModalCreateUser 
                         show={showModalCreateUser}
                         setShow={setShowModalCreateUser}
                         fetchListUser={fetchListUser}
+                    />
+                    <ModalUpdateUser 
+                        show={showModalUpdateUser}
+                        setShow={setShowModalUpdateUser}
+                        dataUpdate={dataUpdate}
                     />
                 </div>
             </div>
