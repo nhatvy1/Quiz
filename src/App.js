68cb5d6bb353b9e1ce969f7767a08472e6693237
nhatvy1 <86657972+nhatvy1@users.noreply.github.com>
2023-05-11 15:59:35 +0700

    redux

--- src/App.js
+++ src/App.js
@@ -16,20 +16,19 @@ class App extends React.Component {
 }
 
 // const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
+// 	const count = useSelector(state => state.counter.count);
+// 	const dispatch = useDispatch();
 
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <MyComponent />
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
-//   );
+// 	return (
+// 		<div className="App">
+// 			<header className="App-header">
+// 			<img src={logo} className="App-logo" alt="logo" />
+// 			<div>Count = {count}</div>
+// 			<button onClick={() => dispatch(increaseCounter())}>Increase</button>
+// 			<button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+// 			</header>
+// 		</div>
+// 	);
 // }
 
 export default App;
